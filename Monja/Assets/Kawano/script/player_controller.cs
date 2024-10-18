52de8cb5018851b7dd379677c25eb9dafeaf5467
KawanoTakumi <h1202416001@hiratagakuen.onmicrsoft.com>
2024-10-19 06:13:24 +0900

    スクリプトを変更しました

--- Monja/Assets/Kawano/script/player_controller.cs
+++ Monja/Assets/Kawano/script/player_controller.cs
@@ -8,18 +8,38 @@ public class player_controller : MonoBehaviour
     public int HP = 100;
     public int MP = 100;
     public int Attack = 50;
-    public int Armor = 50;
-    public int magic = 50;
-    public int magic_armor = 50;
+    public int Diffence = 50;
+    public int Magic = 50;
+    public int Magic_Diffence = 50;
     // Start is called before the first frame update
     void Start()
     {
-        HP = 100; MP = 100; Attack = 50; Armor = 50; magic = 50;magic_armor = 50;
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void Attack_act()
+    {
+
+    }
+
+    public void Diffence_act()
+    {
+
+    }
+
+    public void Magic_act()
+    {
+
+    }
+
+    public void Magic_diffence_act()
+    {
+
     }
 }
