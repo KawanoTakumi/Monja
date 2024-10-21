f692d3e51845a99f316c1de8245cafff7d0f3124
kishitennma <h1202419012@hiratagakuen.onmicrosoft.com>
2024-10-21 10:28:49 +0900

    no message

---
+++ Monja/Assets/Kishi/Enemy_controller.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_controller : MonoBehaviour
+{
+    public int HP = 100;
+    public int MP = 100;
+    public int Attack = 50;
+    public int Diffence = 50;
+    public int Magic = 50;
+    public int Magic_Diffence = 50;
+    public bool Enemy_turn = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
