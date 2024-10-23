0a7215c44d36db21eacd25169ba59cc037a2ffeb
Rui <h1202415020@hiratagakuen.onmicrosoft.com>
2024-10-23 16:31:46 +0900

    10.23日終了

---
+++ Monja/Assets/Ishizuka/Slot.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Slot : MonoBehaviour
+{
+    public Image icon;
+    public GameObject removeButton;
+    Item item;
+    //アイテムを追加する
+    public void AddItem(Item newItem)
+    {
+        item = newItem;
+        icon.sprite = newItem.icon;
+        removeButton.SetActive(true);
+    }
+    //アイテムを取り除く
+    public void ClearItem()
+    {
+        item = null;
+        icon.sprite = null;
+        removeButton.SetActive(false);
+    }
+    //アイテムの消去ボタン
+    public void OnRemoveButton()
+    {
+        Inventry.instance.Remove(item);
+    }
+    //アイテムの使用ボタン
+    public void UseItem()
+    {
+        if (item == null)
+        {
+            return;
+        }
+        item.Use();
+    }
+}
