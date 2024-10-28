d1449fe89f9b623678e5ceaeb04c838fa7e0d39e
Ruirui114 <h1202415020@hiratagakuen.onmicrosoft.com>
2024-10-28 20:37:36 +0900

    説明文オワタ

--- Monja/Assets/Kawano/script/Item_Manager.cs
+++ Monja/Assets/Kawano/script/Item_Manager.cs
@@ -11,19 +11,27 @@ public class Item_Manager : MonoBehaviour
     {
         if (button == CompareTag("bowlingball"))
         {
-            text.text = "aaa";
+            text.text = "ボーリングの玉                        ボーリングに使われていた玉。少し重い。 物理攻撃力を20上昇させ、物理防御力を20減少させる";
         }
         else if (button == CompareTag("radio"))
         {
-            text.text = "bbb";
+            text.text = "ラジオ 音響を無線通信で傍聴できる機械。どこかで体操が始まる。 １ターンごとに防御力を10増加させ、HPを5減少させる";
         }
         else if (button == CompareTag("healdrink"))
         {
-            text.text = "ccc";
+            text.text = "LIFE                                                                 ジュース缶ジュース。 体力を２５％回復する";
         }
         else if (button == CompareTag("hourglass"))
         {
-            text.text = "ddd";
+            text.text = "砂時計                                                 砂を使った時計。時間は有限。 １ターンごとに攻撃力を10上昇させ、体力を5減少させる";
+        } 
+        else if (button == CompareTag("cd"))
+        {
+            text.text = "CD               映像や音楽を書き込むことができる。投げても痛そう。 物理防御力が高いほど、物理攻撃力を上昇させる";
+        }
+        else if (button == CompareTag("CDplayer"))
+        {
+            text.text = "CDプレーヤー CDをセットすることで音楽を聴ける。CDはどこ？ 物理防御力を20上昇させ、物理攻撃力を20減少させる";
         }
 
     }
