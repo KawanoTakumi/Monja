3bcedb5a488a18ac2cfe7972bda59dad41ec73e6
Rui <h1202415020@hiratagakuen.onmicrosoft.com>
2024-10-29 12:31:31 +0900

    a

--- Monja/Assets/Kawano/script/Item_Manager.cs
+++ Monja/Assets/Kawano/script/Item_Manager.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 using UnityEngine.UI;
 public class Item_Manager : MonoBehaviour
 {
+
     public Button button;//ボタン
     public Text text;//説明文
 
+<<<<<<< HEAD
     void Start()
     {  
     //Button button = GetComponent<Button>();
@@ -15,32 +17,39 @@ public class Item_Manager : MonoBehaviour
         
         
     }
+=======
+    public void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+>>>>>>> dc4147b0b0b0ad2033fff2a0b3372fa8a13b5e99
     public void Hit_button()
     {
 
         if (button == CompareTag("bowlingball"))
         {
-            text.text = "ボーリングの玉                        ボーリングに使われていた玉。少し重い。 物理攻撃力を20上昇させ、物理防御力を20減少させる";
+            text.text = "ボーリングの玉\nボーリングに使われていた玉。少し重い。\n物理攻撃力を20上昇させ、物理防御力を20減少させる";
         }
         else if (button == CompareTag("radio"))
         {
-            text.text = "ラジオ 音響を無線通信で傍聴できる機械。どこかで体操が始まる。 １ターンごとに防御力を10増加させ、HPを5減少させる";
+            text.text = "ラジオ\n音響を無線通信で傍聴できる機械。どこかで体操が始まる。\n１ターンごとに防御力を10増加させ、HPを5減少させる";
         }
         else if (button == CompareTag("healdrink"))
         {
-            text.text = "LIFE                                                                 ジュース缶ジュース。 体力を２５％回復する";
+            text.text = "LIFE\nジュース缶ジュース。\n体力を２５％回復する";
         }
         else if (button == CompareTag("hourglass"))
         {
-            text.text = "砂時計                                                 砂を使った時計。時間は有限。 １ターンごとに攻撃力を10上昇させ、体力を5減少させる";
+            text.text = "砂時計\n砂を使った時計。時間は有限。\n１ターンごとに攻撃力を10上昇させ、体力を5減少させる";
         } 
         else if (button == CompareTag("cd"))
         {
-            text.text = "CD               映像や音楽を書き込むことができる。投げても痛そう。 物理防御力が高いほど、物理攻撃力を上昇させる";
+            text.text = "CD\n映像や音楽を書き込むことができる。投げても痛そう。\n物理防御力が高いほど、物理攻撃力を上昇させる";
         }
         else if (button == CompareTag("CDplayer"))
         {
-            text.text = "CDプレーヤー CDをセットすることで音楽を聴ける。CDはどこ？ 物理防御力を20上昇させ、物理攻撃力を20減少させる";
+            text.text = "CDプレーヤー\nCDをセットすることで音楽を聴ける。CDはどこ？\n物理防御力を20上昇させ、物理攻撃力を20減少させる";
         }
 
     }
