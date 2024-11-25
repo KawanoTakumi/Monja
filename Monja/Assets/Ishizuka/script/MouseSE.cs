bc923876c9c32be310888c89cffed446d9c27117
KawanoTakumi <h1202416001@hiratagakuen.onmicrosoft.com>
2024-11-25 10:10:51 +0900

    no message

--- Monja/Assets/Ishizuka/script/MouseSE.cs
+++ Monja/Assets/Ishizuka/script/MouseSE.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class MouseSE : MonoBehaviour
 {
     private new AudioSource audio;
+    public AudioSource button_Audio;//ボタン用のSE
     public AudioClip Sound;
     bool first_hit = false;//最初にオブジェクトに乗った時だけ反応させる
 
@@ -13,15 +14,15 @@ public class MouseSE : MonoBehaviour
     {
         if (first_hit == false)
         {
-            Debug.Log("マウスがオブジェクトに乗っています");
-            GetComponent<AudioSource>().Play();  // 効果音を鳴らす
+            //Debug.Log("マウスがオブジェクトに乗っています");
+            button_Audio.Play();  // 効果音を鳴らす
             first_hit = true;
         }
     }
     //マウスがUIから離れたとき
     public void OnMouseExit()
     {
-        Debug.Log("マウスがオブジェクトから離れました。");
+        //Debug.Log("マウスがオブジェクトから離れました。");
         first_hit = false;
     }
 }
