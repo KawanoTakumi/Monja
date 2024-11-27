bf8f8b7c47ceadb3b8d02500e61e8e1fa810505d
Rui <h1202415020@hiratagakuen.onmicrosoft.com>
2024-11-27 18:02:02 +0900

    no message

--- Monja/Assets/Ishizuka/script/deray.cs
+++ Monja/Assets/Ishizuka/script/deray.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class deray : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
